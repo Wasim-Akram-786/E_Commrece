9667efa7e3b173338bfbe5f80c7cd91431122258
wasim <Wasim15489@gmail.com>
2022-04-24 01:49:19 +0530

    added POM login baseclass

--- POM/LoginPage.py
+++ POM/LoginPage.py
@@ -1,1 +1,24 @@
-import Selenium
+import selenium
+
+# Create class for storing POM based  attributes and action method
+
+class LoginPage:
+    email_xpath="// input[ @ id = 'Email']"
+    password_xpath="//input[@id='Password']"
+    login_xpath="//button[contains(text(),'Log in')]"
+
+    def __init__(self,driver):
+        self.driver=driver
+
+    def setemail(self,email):
+        self.driver.find_element_by_xpath(self.email_xpath).clear()
+        self.driver.find_element_by_xpath(self.email_xpath).send_keys(email)
+
+    def set_password(self,password):
+        self.driver.find_element_by_xpath(self.password_xpath).clear()
+        self.driver.find_element_by_xpath(self.password_xpath).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element_by_xpath(self.login_xpath).click
+
+
