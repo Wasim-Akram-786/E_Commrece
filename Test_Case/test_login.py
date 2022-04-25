1240a2f4e10a6c74feb14d4de3bdb7f7299b8c0f
wasim <Wasim15489@gmail.com>
2022-04-26 00:16:07 +0530

    added POM login baseclass

--- Test_Case/test_login.py
+++ Test_Case/test_login.py
@@ -1,3 +1,13 @@
 
-import pytest
-import selenium
+from POM.LoginPage import LoginPage
+from selenium import webdriver
+
+
+class test_001:
+    email="admin@yourstore.com"
+    password="admin"
+
+
+
+    def test_login(self,setup):
+        self.driver=setup
